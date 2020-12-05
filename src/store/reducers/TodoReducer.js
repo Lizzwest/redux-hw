829d5b26c28723e0bf78fb1d0067968bba8ed42c
Andre Pato <anpato1994@gmail.com>
2020-12-05 14:44:22 -0500

    finish adding reducers

---
+++ src/store/reducers/TodoReducer.js
@@ -0,0 +1,17 @@
+const initialState = {
+  todos: [],
+  newTodo: ''
+}
+
+const TodoReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return { ...state, todos: [...state.todos, action.payload] }
+    case 'NEW_TODO':
+      return { ...state, newTodo: action.payload }
+    default:
+      return { ...state }
+  }
+}
+
+export default TodoReducer
